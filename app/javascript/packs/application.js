5941f15e42e9eec84cf74baef9c6985ac375e303
Praveen Kambale <praveen_kambale@yahoo.com>
2020-03-05 12:35:36 +0530

    Chat message layout and Login page layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+require("jquery")
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -15,3 +15,10 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// Loads all Semantic javascripts
+//= require semantic-ui
+//= require semantic-ui/modal
+//= require semantic-ui/dropdown
+
+
